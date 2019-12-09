b37dcdcbedcb1e795913349dd00f40b614e299b7
sstraw <sean.r.straw@gmail.com>
2019-12-08 21:59:36 -0800

    Problem 2a

--- 02/problem2.go
+++ 02/problem2.go
@@ -23,11 +23,14 @@ func main() {
     scanner := bufio.NewScanner(file)
     scanner.Split(ScanCommas)
 
-    program := make([]int, 0)
+    original_program := make([]int, 0)
     for scanner.Scan() {
         i, _ := strconv.Atoi(scanner.Text())
-        program = append(program, i)
+        original_program = append(original_program, i)
     }
+
+    program := make([]int, len(original_program))
+    copy(program, original_program)
 
     // hardcoded as per instructions, if real input
     if (fname == "input.txt"){
@@ -53,6 +56,32 @@ func main() {
 
     fmt.Printf("Problem 2a: %v\n", program[0])
 
+    for i := 0; i < 100; i++ {
+        for j := 0; j<100; j++ {
+            copy(program, original_program)
+            program[1] = i
+            program[2] = j
+            eip := 0
+            for {
+                opcode := program[eip]
+                if (opcode==1) {
+                    program[program[eip+3]] = program[program[eip+1]] + program[program[eip+2]]
+                } else if (opcode == 2) {
+                    program[program[eip+3]] = program[program[eip+1]] * program[program[eip+2]]
+                } else if (opcode == 99) {
+                    break
+                } else {
+                    break
+                }
+                eip += 4
+            }
+            if program[0] == 19690720 {
+                fmt.Printf("Problem 2b: %v\n", (100*i) + j)
+                i = 100
+                j = 100
+            }
+        }
+    }
 }
 
 func ScanCommas(data []byte, atEOF bool) (advance int, token []byte, err error) {
