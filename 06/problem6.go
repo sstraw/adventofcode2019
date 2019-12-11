855ed4a6383745702d22b5e309381d3f521ff142
sstraw <sean.r.straw@gmail.com>
2019-12-10 21:43:02 -0800

    Problem 6b

--- 06/problem6.go
+++ 06/problem6.go
@@ -8,6 +8,12 @@ import (
     "strings"
 )
 
+type orbit struct {
+    Planet string
+    Center string
+    Orbits []string
+}
+
 func main() {
     fname := "input.txt"
     if (len(os.Args)) == 2 {
@@ -21,11 +27,35 @@ func main() {
 
     scanner := bufio.NewScanner(file)
 
-    orbits := make(map[string][]string)
+    orbits := make(map[string]orbit)
 
+    san_step := ""
+    you_step := ""
     for scanner.Scan() {
+        // x)y : x orbited by y
         t := strings.Split(scanner.Text(), ")")
-        orbits[t[0]] = append(orbits[t[0]], t[1])
+        t_0, t_0_ok := orbits[t[0]]
+        t_1, t_1_ok := orbits[t[1]]
+
+        if !t_0_ok {
+            t_0 = orbit{t[0], "", nil}
+        }
+        t_0.Orbits = append(t_0.Orbits, t[1])
+        orbits[t[0]] = t_0
+
+        if !t_1_ok {
+            t_1 = orbit{t[1], t[0], nil}
+        }
+        if (t_1.Center == "") {
+            t_1.Center = t[0]
+        }
+        orbits[t[1]] = t_1
+
+        if (t[1] == "SAN") {
+            san_step = t[0]
+        } else if (t[1] == "YOU") {
+            you_step = t[0]
+        }
     }
 
     sum := 0
@@ -34,15 +64,43 @@ func main() {
     }
 
     fmt.Printf("Problem 6a: %v\n", sum)
+
+    san_path := []string{san_step}
+    for san_step != "" {
+        o := orbits[san_step]
+        san_path = append(san_path, o.Center)
+        san_step = o.Center
+    }
+
+    you_path := []string{you_step}
+    for you_step != "" {
+        o := orbits[you_step]
+        you_path = append(you_path, o.Center)
+        you_step = o.Center
+    }
+
+    tot := 0
+    for si, sv := range(you_path) {
+        for yi, yv := range(san_path){
+            if sv == yv {
+                tot = si + yi
+                break
+            }
+        }
+        if tot != 0 {
+            break
+        }
+    }
+    fmt.Printf("Problem 6b: %v\n", tot)
 }
 
-func CalculateOrbits(orbits map[string][]string, orbit string) int {
-    s, exists := orbits[orbit]
+func CalculateOrbits(orbits map[string]orbit, orb string) int {
+    s, exists := orbits[orb]
     if (!exists) {
         return 1
     }
     t := 1
-    for _, o := range(s) {
+    for _, o := range(s.Orbits) {
         t += CalculateOrbits(orbits, o)
     }
     return t
