1ca9b8faf01477cfe3f1005b5f4217bb75ed4669
sstraw <sean.r.straw@gmail.com>
2019-12-12 22:29:28 -0800

    Problem 8b

--- 08/problem8.go
+++ 08/problem8.go
@@ -28,11 +28,15 @@ func main() {
     n_zero_digits := -1
     problem_8a := 0
 
+
+    layers := make([][][]int, 0)
     for l := 0; l * image_w * image_h < len(image_raw); l++ {
         c_zero := 0
         s_1 := 0
         s_2 := 0
+        layers = append(layers, make([][]int, 0))
         for y := 0; y < image_h; y++ {
+            layers[l] = append(layers[l], make([]int, 0))
             for x := 0; x < image_w; x++ {
                 i := (l*image_w*image_h)+(y*image_w)+x
                 v := int(image_raw[i])-48
@@ -43,6 +47,7 @@ func main() {
                 } else if v == 2 {
                     s_2 += 1
                 }
+                layers[l][y] = append(layers[l][y], v)
             }
         }
         if (c_zero < n_zero_digits || n_zero_digits == -1) {
@@ -53,4 +58,21 @@ func main() {
     }
     fmt.Println("Problem 8a:", problem_8a)
 
+    n_layers := len(layers)
+    for y := 0; y < image_h; y++ {
+        for x := 0; x < image_w; x++ {
+            for l := 0; l < n_layers; l++ {
+                v := layers[l][y][x]
+                if v != 2 {
+                    if v == 0 {
+                        fmt.Print(" ")
+                    } else {
+                        fmt.Print("X")
+                    }
+                    break
+                }
+            }
+        }
+        fmt.Println("")
+    }
 }
