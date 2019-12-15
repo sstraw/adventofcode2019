e96c53b8e62f8b4da2ba7072c20cc128b78d5862
sstraw <sean.r.straw@gmail.com>
2019-12-14 23:51:03 -1000

    Problem 10b

--- 10/problem10.go
+++ 10/problem10.go
@@ -6,6 +6,7 @@ import (
     "bufio"
     "log"
     "math"
+    "sort"
 )
 
 func main() {
@@ -33,11 +34,12 @@ func main() {
         y += 1
     }
 
+    var m_slopes map[float64][]Asteroid
     max_vis := 0
     for j := 0; j < len(asteroids); j++ {
         //Outer loop testing each asteroid as a command point
         t_a := asteroids[j]
-        slopes := make(map[float64] *struct{}, 0)
+        slopes := make(map[float64] []Asteroid, 0)
 
         for i := 0; i < len(asteroids); i ++ {
             if i == j {
@@ -45,15 +47,42 @@ func main() {
                 continue
             }
             a := asteroids[i]
-            slope := math.Atan2(float64(a.Y - t_a.Y), float64(a.X-t_a.X))
-            slopes[slope] = nil
+            slope := math.Atan2(float64(a.X-t_a.X), float64(a.Y - t_a.Y))
+            if slope > (math.Pi/2) {
+                //slope = -1.5 * math.Pi + (math.Pi-slope)
+            }
+            slopes[slope] = append(slopes[slope], a)
         }
 
         if len(slopes) > max_vis {
             max_vis = len(slopes)
+            m_slopes = slopes
         }
     }
     fmt.Println("Problem 10a:", max_vis)
+
+    // List of keys
+    ks := make([]float64,0)
+    for k, _ := range(m_slopes) {
+        ks = append(ks, k)
+    }
+    sort.Float64s(ks)
+    blasted_roids := 0
+    for i := len(ks); i > 0; i-=1 {
+        target_i := ks[i-1]
+        target := m_slopes[target_i]
+        if len(target) > 0 {
+            blasted_roids += 1
+            if blasted_roids == 200 {
+                fmt.Println("Problem 10b:", target[0].X*100 + target[0].Y)
+                break
+            } else if len(target) > 1 {
+                m_slopes[target_i] = target[1:]
+            } else {
+                m_slopes[target_i] = target[1:]
+            }
+       }
+    }
 }
 
 type Asteroid struct {
