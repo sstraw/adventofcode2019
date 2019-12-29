55535a3c1ea5fc52341245aec08b604a7745bb54
sstraw <sean.r.straw@gmail.com>
2019-12-29 10:47:30 -0800

    Problem 16b

--- 16/problem16.go
+++ 16/problem16.go
@@ -28,10 +28,41 @@ func main() {
         input = append(input, int(r-'0'))
     }
 
+    output := make([]int, len(input))
+    copy(output, input)
     for i := 0; i < 100; i++ {
-        input = Phase(input)
+        output = Phase(output)
     }
-    fmt.Println("Problem 16a:", InputString(input[:8]))
+    fmt.Println("Problem 16a:", InputString(output[:8]))
+
+    msg_offset, _ := strconv.Atoi(scanner.Text()[:7])
+    large_input   := make([]int, len(input)*10000)
+    for i := 0; i < 10000*len(input); i += len(input) {
+        copy(large_input[i:], input)
+    }
+
+
+    // As observed in the subreddit - for all indices 
+    // where i > len/2, the pattern will just be 
+    // 1, 1, 1, 1, etc
+
+    if (msg_offset <= len(large_input)/2 ||
+        msg_offset >= len(large_input)) {
+        log.Fatal("Message offset doesn't match expected")
+    }
+    for n := 0; n < 100; n++ {
+        sum := 0
+        for i := len(large_input) - 1; i >= msg_offset; i-- {
+            //fmt.Println("i: ",i)
+            sum += large_input[i]
+            large_input[i] = sum % 10
+            if large_input[i] < 0 {
+                large_input[i] = large_input[i] * -1
+            }
+        }
+    }
+    str := InputString(large_input[msg_offset:msg_offset+8])
+    fmt.Println("Problem 16b:", str)
 }
 
 func Phase(inp []int) []int {
